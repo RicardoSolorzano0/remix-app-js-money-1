13e9c0c1dd6dac6b498b6781bb3f8635811ad14d
RicardoSolorzano2 <144890516+RicardoSolorzano2@users.noreply.github.com>
2023-11-02 09:02:01 -0600

    resources routes

--- app/routes/expenses.raw.jsx
+++ app/routes/expenses.raw.jsx
@@ -1,3 +1,19 @@
-export default function ExpensesRaw() {
-  return <>Expenses raw</>;
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "first expense",
+    amount: 12.99,
+    date: new Date().toISOString(),
+  },
+  {
+    id: "e2",
+    title: "second expense",
+    amount: 16.99,
+    date: new Date().toISOString(),
+  },
+];
+
+//if only loader in the file, load the data in the viewport
+export function loader() {
+  return DUMMY_EXPENSES;
 }
