5c564c9554fdc4c3fdea1584caef759a9e8eb0f6
RicardoSolorzano2 <144890516+RicardoSolorzano2@users.noreply.github.com>
2023-11-02 17:32:20 -0600

    add file validation data and add code for send data with old format

--- app/routes/expenses.add.jsx
+++ app/routes/expenses.add.jsx
@@ -3,6 +3,7 @@ import ExpenseForm from "../components/expenses/ExpenseForm";
 import Modal from "../components/util/Modal";
 import { addExpense } from "../data/expenses.server";
 import { redirect } from "@remix-run/node";
+import { validateExpenseInput } from "../data/validation.server";
 
 export default function ExpensesAdd() {
   const navigate = useNavigate();
@@ -21,6 +22,13 @@ export default function ExpensesAdd() {
 export async function action({ request }) {
   const formData = await request.formData();
   const expenseData = Object.fromEntries(formData);
+
+  try {
+    validateExpenseInput(expenseData);
+  } catch (err) {
+    return err;
+  }
+
   await addExpense(expenseData);
   return redirect("/expenses");
 }
