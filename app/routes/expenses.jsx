2c0dae51841a27fda3277b8241e149a1126e793d
RicardoSolorzano2 <144890516+RicardoSolorzano2@users.noreply.github.com>
2023-11-03 10:20:02 -0600

    fix main of expenses

--- app/routes/expenses.jsx
+++ app/routes/expenses.jsx
@@ -1,38 +1,41 @@
-import { Link, Outlet, useLoaderData } from "@remix-run/react";
+import { Link, Outlet, useLoaderData, useLocation } from "@remix-run/react";
 import ExpensesList from "../components/expenses/ExpensesList";
 import expensesStyle from "../styles/expenses.css";
 import ExpensesHeader from "../components/navigation/ExpensesHeader";
 import { getExpenses } from "../data/expenses.server";
 
 export default function ExpensesLayout() {
+  const path = useLocation();
   const expenses = useLoaderData();
   const hasExpenses = expenses && expenses.length > 0;
   return (
     <>
       <ExpensesHeader />
       <Outlet />
-      <main>
-        <section id="expenses-actions">
-          <Link to="add">
-            icon
-            <span>Add Expense</span>
-          </Link>
-          <a href="/expenses/raw">
-            icon
-            <span>Load raw data</span>
-          </a>
-        </section>
-
-        {hasExpenses && <ExpensesList expenses={expenses} />}
-        {!hasExpenses && (
-          <section id="no-expenses">
-            <h1>No expenses found</h1>
-            <p>
-              Start <Link to="add">adding some</Link> today
-            </p>
+      {path.pathname !== "/expenses/analysis" ? (
+        <main>
+          <section id="expenses-actions">
+            <Link to="add">
+              icon
+              <span>Add Expense</span>
+            </Link>
+            <a href="/expenses/raw">
+              icon
+              <span>Load raw data</span>
+            </a>
           </section>
-        )}
-      </main>
+
+          {hasExpenses && <ExpensesList expenses={expenses} />}
+          {!hasExpenses && (
+            <section id="no-expenses">
+              <h1>No expenses found</h1>
+              <p>
+                Start <Link to="add">adding some</Link> today
+              </p>
+            </section>
+          )}
+        </main>
+      ) : null}
     </>
   );
 }
